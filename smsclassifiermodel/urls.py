250e0dc091489bbc11a0b3c4ecbd162f0db385d1
Agustinus Theodorus <agustinustheoo@gmail.com>
2019-01-11 16:09:36 +0700

    first commit

--- smsclassifiermodel/urls.py
+++ smsclassifiermodel/urls.py
@@ -3,6 +3,6 @@ from . import views
 from . import sms_classifier_model
 
 urlpatterns = [
-    path('', views.home, name="classify-home"),
-    path('test/', sms_classifier_model.classify_text, name="classify-test"),
+    path('home/', views.home, name="classify-home"),
+    path('', sms_classifier_model.classify_text, name="classify-test"),
 ]
