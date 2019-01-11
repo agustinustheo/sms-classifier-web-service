a0b47ab772f0a9831da5033f6371a011852a39d0
Agustinus Theodorus <agustinustheoo@gmail.com>
2019-01-11 15:52:20 +0700

    added parameter to GET text to be classified

--- smsclassifiermodel/urls.py
+++ smsclassifiermodel/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from . import sms_classifier_model
 
 urlpatterns = [
     path('', views.home, name="classify-home"),
+    path('test/', sms_classifier_model.classify_text, name="classify-test"),
 ]
